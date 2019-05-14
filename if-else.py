49e537fa4e74186cbbfed6f4359e40cf5cb99207
Olalekan Tosin Sogunle <sogunleolalekan@gmail.com>
2019-05-14 19:03:08 +0200

    https://www.hackerrank.com/challenges/py-if-else/problem

---
+++ if-else.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+if(N%2 > 0):
+    print('Weird')
+else:
+    if N in range(2, 6):
+        print('Not Weird')
+    elif N in range(6, 21):
+        print('Weird')
+    elif N > 20:
+        print('Not Weird')
